9211d0b1e233c7e4c23000c21b709b88dc57fe5f
jjsmx24@gmail.com <jjsmx24@gmail.com>
2023-09-23 18:23:38 +0100

    class main added, class Employee complete

--- src/software/Employee.java
+++ src/software/Employee.java
@@ -1,4 +1,41 @@
 package software;
 
 public class Employee {
+    private String Name;
+    private int Salary;
+    private int NetSalary;
+    public Employee(String name, int salary){
+        this.Name = name;
+        this.Salary = salary;
+        this.SetNetSalary();
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(int salary) {
+        Salary = salary;
+    }
+
+    private void SetNetSalary() {
+        this.NetSalary = (int)(this.Salary * 0.85);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "Name='" + Name + '\'' +
+                ", Salary=" + Salary +
+                ", NetSalary=" + NetSalary +
+                '}';
+    }
 }
---
+++ src/software/Main.java
@@ -0,0 +1,8 @@
+package software;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee humpty = new Employee("Humpty", 1500);
+        System.out.println(humpty);
+    }
+}
